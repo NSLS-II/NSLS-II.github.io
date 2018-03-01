1049665544965792967d642b7a221ca128c59b49
Travis CI User <travis@example.org>
2018-03-01 02:14:14 +0000

    Update docs after building Travis build 1051 of NSLS-II/ophyd
    The docs were built from the branch 'master' against the commit
    fbe5896c307f641f293f2995a4ec383bef0e0f9b.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/NSLS-II/ophyd/jobs/347572700.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --deploy-repo NSLS-II/NSLS-II.github.io --deploy-branch-name master databroker

--- databroker/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0.10.0.post1+g452b112',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
