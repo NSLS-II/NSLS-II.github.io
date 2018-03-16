c4de958a72263fa87f798af55b013423b8c4b44d
Travis CI User <travis@example.org>
2018-03-16 10:54:44 -0400

    Update docs after building Travis build 968 of NSLS-II/databroker
    The docs were built from the branch 'master' against the commit
    6fc5c9f7bad10d54205a07be5d71b4c36f155936.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/NSLS-II/databroker/jobs/354350634.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --deploy-repo NSLS-II/NSLS-II.github.io --deploy-branch-name master databroker

--- databroker/_static/documentation_options.js
+++ databroker/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.10.0.post10+g4fda93e',
+    VERSION: '0.10.0.post17+g6fc5c9f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
