3e2f0c10645e579aacf4a9d09fe236cb42493876
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-02-15 20:19:20 +0000

    Update docs after building Travis build 45 of NSLS-II/intake-bluesky
    The docs were built from the branch 'master' against the commit
    20717f7e353d67997663206e64a910c8bdaeab7b.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/NSLS-II/intake-bluesky/jobs/493937145.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html --deploy-repo NSLS-II/NSLS-II.github.io intake-bluesky

--- intake-bluesky/_static/copybutton.js
+++ intake-bluesky/_static/copybutton.js
@@ -1,59 +1,95 @@
-function addCopyButtonToCode(){
-  // get all code elements
-  var allCodeBlocksElements = $( "div.highlight pre" );
-
-  // For each element, do the following steps
-  allCodeBlocksElements.each(function(ii) {
-  // define a unique id for this element and add it
-  var currentId = "codeblock" + (ii + 1);
-  $(this).attr('id', currentId);
-
-  // create a button that's configured for clipboard.js
-  // point it to the text that's in this code block
-  // add the button just after the text in the code block w/ jquery
-  var clipButton = '<button class="btn copybtn" data-clipboard-target="#' + currentId + '"><i class="far fa-copy" alt="Copy to clipboard"></i></button>';
-     $(this).after(clipButton);
-  });
-
-  // Tooltip functions
-  function setTooltip(btn, message) {
-    $(btn).tooltip('hide')
-      .attr('data-original-title', message)
-      .tooltip('show');
+// Localization support
+const messages = {
+  'en': {
+    'copy': 'Copy',
+    'copy_to_clipboard': 'Copy to clipboard',
+    'copy_success': 'Copied!',
+    'copy_failure': 'Failed to copy',
+  },
+  'es' : {
+    'copy': 'Copiar',
+    'copy_to_clipboard': 'Copiar al portapapeles',
+    'copy_success': '¡Copiado!',
+    'copy_failure': 'Error al copiar',
+  },
+  'de' : {
+    'copy': 'Kopieren',
+    'copy_to_clipboard': 'In die Zwischenablage kopieren',
+    'copy_success': 'Kopiert!',
+    'copy_failure': 'Fehler beim Kopieren',
   }
-
-  function hideTooltip(btn) {
-    setTimeout(function() {
-      $(btn).tooltip('hide');
-    }, 800);
-  }
-
-  $('.btn').tooltip({
-    trigger: 'click',
-    placement: 'bottom'
-  });
-
-  // tell clipboard.js to look for clicks that match this query
-  var clipboard = new Clipboard('.btn');
-
-  function clearSelection() {
-   if (window.getSelection) {window.getSelection().removeAllRanges();}
-   else if (document.selection) {document.selection.empty();}
-  }
-
-  clipboard.on('success', function(e) {
-    clearSelection();
-    setTooltip(e.trigger, 'Copied!');
-    hideTooltip(e.trigger);
-  });
-
-  clipboard.on('error', function(e) {
-    setTooltip(e.trigger, 'Failed!');
-    hideTooltip(e.trigger);
-  });
-
 }
-$(document).ready(function () {
-// Once the DOM is loaded for the page, attach clipboard buttons
-addCopyButtonToCode();
-});
+
+let locale = 'en'
+if( document.documentElement.lang !== undefined
+    && messages[document.documentElement.lang] !== undefined ) {
+  locale = document.documentElement.lang
+}
+
+/**
+ * Set up copy/paste for code blocks
+ */
+
+const runWhenDOMLoaded = cb => {
+  if (document.readyState != 'loading') {
+    cb()
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', cb)
+  } else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState == 'complete') cb()
+    })
+  }
+}
+
+const codeCellId = index => `codecell${index}`
+
+// Clears selected text since ClipboardJS will select the text when copying
+const clearSelection = () => {
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges()
+  } else if (document.selection) {
+    document.selection.empty()
+  }
+}
+
+// Changes tooltip text for two seconds, then changes it back
+const temporarilyChangeTooltip = (el, newText) => {
+  const oldText = el.getAttribute('data-tooltip')
+  el.setAttribute('data-tooltip', newText)
+  setTimeout(() => el.setAttribute('data-tooltip', oldText), 2000)
+}
+
+const addCopyButtonToCodeCells = () => {
+  // If ClipboardJS hasn't loaded, wait a bit and try again. This
+  // happens because we load ClipboardJS asynchronously.
+  if (window.ClipboardJS === undefined) {
+    setTimeout(addCopyButtonToCodeCells, 250)
+    return
+  }
+
+  const codeCells = document.querySelectorAll('div.highlight pre')
+  codeCells.forEach((codeCell, index) => {
+    const id = codeCellId(index)
+    codeCell.setAttribute('id', id)
+    const pre_bg = getComputedStyle(codeCell).backgroundColor;
+
+    const clipboardButton = id =>
+    `<a class="copybtn o-tooltip--left" style="background-color: ${pre_bg}" data-tooltip="${messages[locale]['copy']}" data-clipboard-target="#${id}">
+      <img src="https://gitcdn.xyz/repo/choldgraf/sphinx-copybutton/master/sphinx_copybutton/_static/copy-button.svg" alt="${messages[locale]['copy_to_clipboard']}">
+    </a>`
+    codeCell.insertAdjacentHTML('afterend', clipboardButton(id))
+  })
+
+  const clipboard = new ClipboardJS('.copybtn')
+  clipboard.on('success', event => {
+    clearSelection()
+    temporarilyChangeTooltip(event.trigger, messages[locale]['copy_success'])
+  })
+
+  clipboard.on('error', event => {
+    temporarilyChangeTooltip(event.trigger, messages[locale]['copy_failure'])
+  })
+}
+
+runWhenDOMLoaded(addCopyButtonToCodeCells)
