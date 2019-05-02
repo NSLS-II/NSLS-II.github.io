b024e167654f920261c61a63c0fa86f652dcadc5
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-05-02 01:19:54 +0000

    Update docs after building Travis build 203 of NSLS-II/event-model
    The docs were built from the branch 'master' against the commit
    4784a3e16613bf7d0cbb1e3b0970d473154ff74f.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/NSLS-II/event-model/jobs/527106673.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html --deploy-repo NSLS-II/NSLS-II.github.io event-model

--- event-model/_static/documentation_options.js
+++ event-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.9.0a3.post7+g705d3f8',
+    VERSION: '1.9.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
