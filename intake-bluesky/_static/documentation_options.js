c42f6d85f8d5191c0c72530996399839844a9144
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-05-13 14:01:19 +0000

    Update docs after building Travis build 147 of bluesky/intake-bluesky
    The docs were built from the branch 'master' against the commit
    04cc685ec9266339c920a66b946d8787baf562ef.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bluesky/intake-bluesky/jobs/531798562.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html --deploy-repo NSLS-II/NSLS-II.github.io intake-bluesky

--- intake-bluesky/_static/documentation_options.js
+++ intake-bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a5.post6+gbe43b7e',
+    VERSION: '0.1.0a5.post8+g04cc685',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
