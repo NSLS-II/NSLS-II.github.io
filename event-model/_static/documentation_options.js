ad65eb6783bf124e88cae8a1a03b3f938409efa3
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-29 13:33:15 +0000

    Update docs after building Travis build 562 of bluesky/event-model
    The docs were built from the branch 'master' against the commit
    20e3b54efc2f94ec9e6119a5026cd35bde540e76.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bluesky/event-model/jobs/643379856.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html --deploy-repo NSLS-II/NSLS-II.github.io event-model

--- event-model/_static/documentation_options.js
+++ event-model/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.13.0.post19+gda38f5d',
+    VERSION: '1.13.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
