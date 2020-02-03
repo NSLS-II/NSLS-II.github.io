a14c60049ed7df4310fb900458542e29d9d661ae
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-02-03 19:34:28 +0000

    Update docs after building Travis build 127 of NSLS-II/amostra
    The docs were built from the branch 'master' against the commit
    7c6bbb3ceb4fa2f10b9c7447f214f51ff3f2f2e7.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/NSLS-II/amostra/jobs/645619862.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --deploy-repo NSLS-II/NSLS-II.github.io --deploy-branch-name master amostra

--- amostra/_static/documentation_options.js
+++ amostra/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.post17+g66b9c52',
+    VERSION: '1.0.1.post38+g7c6bbb3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
