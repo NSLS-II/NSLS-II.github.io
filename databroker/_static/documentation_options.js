97b3ffe9ca512e83c743aeed9c307fcbe7ccc7cd
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-03-31 16:02:27 -0400

    Update docs after building Travis build 2056 of bluesky/databroker
    The docs were built from the branch 'master' against the commit
    dec419514be821b0d4df9ff61e9ea6c50517be82.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bluesky/databroker/jobs/669393222.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --deploy-repo NSLS-II/NSLS-II.github.io --deploy-branch-name master databroker

--- databroker/_static/documentation_options.js
+++ databroker/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0.post22+g2f7c390',
+    VERSION: '1.0.0.post27+gdec4195',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
