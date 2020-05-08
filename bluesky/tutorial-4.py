9e39160ace1d2659f60790adbeecb8b6a42b168d
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-05-08 17:47:27 +0000

    Update docs after building Travis build 3495 of bluesky/bluesky
    The docs were built from the branch 'master' against the commit
    9adc76fc1297981cf34f6e3f957fd332a8d5f191.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bluesky/bluesky/jobs/684778915.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --deploy-repo NSLS-II/NSLS-II.github.io --deploy-branch-name master bluesky

--- bluesky/tutorial-4.py
+++ bluesky/tutorial-4.py
@@ -3,12 +3,12 @@ from ophyd.sim import motor1, motor2, det
 from bluesky.plans import grid_scan
 import matplotlib.pyplot as plt
 
-true_plan = grid_scan([det], motor1, -5, 5, 10, motor2, -7, 7, 15, True)
-false_plan = grid_scan([det], motor1, -5, 5, 10, motor2, -7, 7, 15, False)
+snaked = grid_scan([det], motor1, -5, 5, 10, motor2, -7, 7, 15, snake_axes=True)
+not_snaked = grid_scan([det], motor1, -5, 5, 10, motor2, -7, 7, 15)
 
 fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-plot_raster_path(true_plan, 'motor1', 'motor2', probe_size=.3, ax=ax1)
-plot_raster_path(false_plan, 'motor1', 'motor2', probe_size=.3, ax=ax2)
+plot_raster_path(snaked, 'motor1', 'motor2', probe_size=.3, ax=ax1)
+plot_raster_path(not_snaked, 'motor1', 'motor2', probe_size=.3, ax=ax2)
 ax1.set_title('True')
 ax2.set_title('False')
 ax1.set_xlim(-6, 6)
