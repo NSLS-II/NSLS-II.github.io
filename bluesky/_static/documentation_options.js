7eb97d56210e1e4c750cf9963cdeecfa968fcd66
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-08-25 21:31:54 +0000

    Update docs after building Travis build 3722 of bluesky/bluesky
    The docs were built from the branch 'master' against the commit
    ce18eee47b93a0ff8058e2b4840120b8e84dbec3.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bluesky/bluesky/jobs/721143391.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --deploy-repo NSLS-II/NSLS-II.github.io --deploy-branch-name master bluesky

--- bluesky/_static/documentation_options.js
+++ bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.5.post6+g0171956',
+    VERSION: '1.6.5.post14+gce18eee',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
