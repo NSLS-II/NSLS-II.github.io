212d974724f189e8310ba5c9e41f6b34c53c2750
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-09-25 00:07:17 +0000

    Update docs after building Travis build 3189 of caproto/caproto
    The docs were built from the branch 'master' against the commit
    0fd48cef2e7022328393c30672388b25039f16e2.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/caproto/caproto/jobs/390724082.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --deploy-repo NSLS-II/NSLS-II.github.io --deploy-branch-name master caproto

--- caproto/_static/documentation_options.js
+++ caproto/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.0+42.gc7ef7be.dirty',
+    VERSION: '0.6.0+52.g0fd48ce.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
