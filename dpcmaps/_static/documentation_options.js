cf8d61e52538457a53c40cc0578c8779cc686941
dmgav <dmgav@users.noreply.github.com>
2021-10-01 22:03:15 +0000

    deploy: NSLS-II/dpcmaps@2e8fde53b1117df5c450434b4ee99c82b86a40ef

--- dpcmaps/_static/documentation_options.js
+++ dpcmaps/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g7e5181a',
+    VERSION: '0.post1+g2e8fde5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
