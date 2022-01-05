86056872fb1665cfe4f70769895174559ee3f81d
maffettone <maffettone@users.noreply.github.com>
2022-01-05 20:27:29 +0000

    deploy: NSLS-II/constrained-matrix-factorization@e4d8ee13788373ec3075c0c38c3fa369b2708a1e

--- constrained-matrix-factorization/_static/documentation_options.js
+++ constrained-matrix-factorization/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.post3+g3dfb634',
+    VERSION: '0.1.1.post11+ge4d8ee1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
