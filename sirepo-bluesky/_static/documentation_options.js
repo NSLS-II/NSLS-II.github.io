5fabb9bd86af95ea2bfeb784907fbe3ab8a753c3
mrakitin <mrakitin@users.noreply.github.com>
2022-03-07 23:52:33 +0000

    deploy: NSLS-II/sirepo-bluesky@bf47eb7a0884af2551d201a12998d875fc280609

--- sirepo-bluesky/_static/documentation_options.js
+++ sirepo-bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g638af15',
+    VERSION: '0.post1+gbf47eb7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
