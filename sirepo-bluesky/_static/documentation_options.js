6a96216805e8a1760c3028bde6b579ac874bdd0f
thomaswmorris <thomaswmorris@users.noreply.github.com>
2022-10-14 00:13:32 +0000

    deploy: NSLS-II/sirepo-bluesky@8efad985e444bc6097738a61e58de18a98ca0928

--- sirepo-bluesky/_static/documentation_options.js
+++ sirepo-bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.3.post92+ge18aae8',
+    VERSION: '0.4.3.post97+g8efad98',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
