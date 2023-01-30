d998d6e96c5651db09918d95e05b96e9fc5c982e
AbbyGi <AbbyGi@users.noreply.github.com>
2023-01-30 21:26:33 +0000

    deploy: NSLS-II/sirepo-bluesky@77f157af48ee2d9ac13575e9b2daa8b6be26a8ee

--- sirepo-bluesky/_static/documentation_options.js
+++ sirepo-bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.0.post19+g60c532a',
+    VERSION: '0.5.0.post23+g77f157a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
