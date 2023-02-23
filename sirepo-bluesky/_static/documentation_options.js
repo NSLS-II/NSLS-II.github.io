3efb06908b32915aacb8a7bdc15498e9e1e79efa
jklynch <jklynch@users.noreply.github.com>
2023-02-23 14:26:55 +0000

    deploy: NSLS-II/sirepo-bluesky@95e575decfcf98cdbc7f3299a64a95771fd46948

--- sirepo-bluesky/_static/documentation_options.js
+++ sirepo-bluesky/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.0.post30+gb3e4d90',
+    VERSION: '0.5.0.post33+g95e575d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
