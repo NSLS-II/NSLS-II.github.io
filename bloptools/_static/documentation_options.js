ef54a118dd3349881748246eeae859ee4b3007ff
mrakitin <mrakitin@users.noreply.github.com>
2023-04-18 14:38:07 +0000

    deploy: NSLS-II/bloptools@df074747d9b4be2c60c58d99ee15bd3a7cea2394

--- bloptools/_static/documentation_options.js
+++ bloptools/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post124+g435bf12',
+    VERSION: '0.post137+gdf07474',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
