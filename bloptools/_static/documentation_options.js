00db690c8a4d1af58c8a8b46ffbedef152e130c6
AbbyGi <AbbyGi@users.noreply.github.com>
2023-06-06 14:35:36 +0000

    deploy: NSLS-II/bloptools@5ef478b4ac70e4effed0be862cdcc4e6c854d927

--- bloptools/_static/documentation_options.js
+++ bloptools/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0.post22+ga558103',
+    VERSION: '0.2.0.post24+g5ef478b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
