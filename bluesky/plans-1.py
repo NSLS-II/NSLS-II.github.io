2b77e3b3dcbe3e03c689f6ea105cb715f91e85bb
Edmund Blomley <edmund.blomley@kit.edu>
2024-06-24 15:34:18 +0200

    Remove obsolete bluesky documentation, link to new one

--- bluesky/plans-1.py
+++
@@ -1,8 +0,0 @@
-from bluesky import RunEngine
-from bluesky.plans import count
-from ophyd.sim import noisy_det
-from bluesky.callbacks.best_effort import BestEffortCallback
-bec = BestEffortCallback()
-RE = RunEngine({})
-RE.subscribe(bec)
-RE(count([noisy_det], num=5))
--- bluesky/plans-2.py
+++
@@ -1,8 +0,0 @@
-from bluesky import RunEngine
-from bluesky.plans import scan
-from ophyd.sim import det, motor
-RE = RunEngine({})
-from bluesky.callbacks.best_effort import BestEffortCallback
-bec = BestEffortCallback()
-RE.subscribe(bec)
-RE(scan([det], motor, 1, 5, 5))
